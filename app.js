f5c6ef8af4a0a986c8db5339233f4510e40d3b00
Bogdan Kyrychenko <bogdan50e@gmail.com>
2015-06-12 02:25:49 +0300

    proxy NYTimes api

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var proxy = require('express-http-proxy');
 var config = require(path.join(__dirname, 'config'));
 
+// Express config
 var app = express();
 app.use('/public', express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// routes
+app.use('/api_nytimes', proxy('api.nytimes.com', {
+	  forwardPath: function(req, res) {
+	    return require('url').parse(req.url).path;
+	  }
+	}));
 app.get('/',function(req,res){
 	res.render('index');
 });
 
+// start server
 http.createServer(app).listen(config.get('port'), config.get('ip'), function(){
 	console.log('Server running at http://' + config.get('ip') + ':' + config.get('port') + '/');
 });
